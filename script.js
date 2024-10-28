2614a65aed0e0dc56430a48e665d57f902fc6da4
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-10-28 16:33:28 -0400

    fixed the ordering of things and also the json files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scheduleUrl = 'https://api.npoint.io/ee538edca0550b4db30f'
+const scheduleUrl = 'https://api.npoint.io/ebaae45b7ae32de21578'
 
 const bellSchedule = {
     1: { start: '8:24 AM', end: '9:31 AM' },
@@ -26,23 +26,60 @@ $('#submitDay').on('click', function () {
             )
             $('#scheduleList').empty()
 
+            let i = 1
             daySchedule.forEach(element => {
-                const per = element.period
-                const time = bellSchedule[per]
+                const time = bellSchedule[i]
                 let htmlString = `
-                <tr>
-                <td>${per}</td>
+                <tr id="row${i}">
                 <td>${time.start} to ${time.end}</td>
-                <td>${element.class}</td>
-                <td>${element.teacher}</td>
-                <td>${element.room}</td>
                 </tr>
                 `
                 $('#scheduleList').append(htmlString);
+                let htmlString2 = `
+                <td>${element.period}</td>
+                <td>${element.class}</td>
+                <td>${element.teacher}</td>
+                <td>${element.room}</td>
+                `
+                addToArray(element.period, htmlString2)
+                i++
             });
         },
         error: function () {
-
+            alert('Error occurred while retrieving data, please reload the page and try again.')
         }
     });
 })
+
+let classArray = []
+
+function addToArray(period, info) {
+    classArray.push({ period: period, info: info })
+    if (classArray.length === 5) {
+        let order = []
+        if ($('#dayInput').val().toUpperCase() === 'A') {
+            order = [1, 2, 3, 5, 6]
+        } else if($('#dayInput').val().toUpperCase() === 'B') {
+            order = [4, 1, 2, 7, 5]
+        } else if($('#dayInput').val().toUpperCase() === 'C') {
+            order = [3, 4, 1, 6, 7]
+        } else if($('#dayInput').val().toUpperCase() === 'D') {
+            order = [2, 3, 4, 5, 6]
+        } else if($('#dayInput').val().toUpperCase() === 'E') {
+            order = [1, 2, 3, 7, 5]
+        } else if($('#dayInput').val().toUpperCase() === 'F') {
+            order = [4, 1, 2, 6, 7]
+        } else if($('#dayInput').val().toUpperCase() === 'G') {
+            order = [3, 4, 7, 5, 6]
+        }
+        classArray.sort((a, b) => {
+            return order.indexOf(a.period) - order.indexOf(b.period);
+        });
+        let i = 1
+        classArray.forEach(element => {
+            $(`#row${i}`).append(element.info); // this line is the problem
+            i++
+        });
+        classArray = []
+    }
+}
